4857bc5d882a00a0f990d53976ea3bbca3d5740c
ethomascappello <ethomascappello@Marist-HNSQXQ2.necsd.net>
2019-08-08 17:04:57 -0400

    ScannerOneGood

---
+++ ScannerOne/src/ScannerPrac.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ScannerPrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*Milestone 1: Create code which prompts user for input, 
+ * taking three int inputs (user ratings), followed by a pair of doubles
+ *  (focus group ratings) and a single double (critic rating). 
+ All of these should be stored in appropriate variables.
+ */
